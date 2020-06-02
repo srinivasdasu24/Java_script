0af82c98253d228375eb82f68899061a2217f508
srinivas dasu <srinivas.dasucse@gmail.com>
2020-06-02 17:31:57 +0530

    Simple static website and node.js application

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
+
+
+console.log('Here\'s is hidden message')
